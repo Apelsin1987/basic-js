1c91fd3ad7c01c7b6f8969d55f8d90a18cf4a909
Konstantin Si <apelsin1987@gmail.com>
2023-12-24 17:24:37 +0100

    solved hanoi-tower, matrix-elements-sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function getMatrixElementsSum(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let sum = 0;
+  let zerosArr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    let row = matrix[i];
+    for (let j = 0; j < row.length; j++) {
+      let elem = row[j];
+      if (elem === 0) {
+        if (!zerosArr.includes(j)) {
+          zerosArr.push(j);
+        }
+      } else {
+        if (!zerosArr.includes(j)) {
+          sum += elem;
+        }
+      }
+    }
+  }
+  return sum;
 }
 
 module.exports = {
