c8fb4a2ad728ceca5c912ebe694dded2c3785e3f
Konstantin Si <apelsin1987@gmail.com>
2023-12-24 19:52:55 +0100

    solved simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,29 +1,57 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.value.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!this.value) {
+      this.value = [];
+    }
+    value = String(value);
+    this.value.push(value);
+
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!this.value || typeof position !== 'number' || position < 1 || position > this.value.length) {
+      this.value = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }
+    this.value.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!this.value) {
+      return;
+    }
+    this.value.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!this.value) {
+      return '';
+    }
+    let chainStr = '';
+    for (let i = 0; i < this.value.length; i++) {
+      chainStr += `( ${this.value[i]} )~~`;
+    }
+    this.value = [];
+    return chainStr.slice(0, -2);
   }
 };
 
