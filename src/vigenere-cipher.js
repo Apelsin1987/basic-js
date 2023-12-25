b45b309aa3b01966316de8ed58a3335233e8f4ea
Konstantin Si <apelsin1987@gmail.com>
2023-12-25 08:29:18 +0100

    solved vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  messageEncDec(message, key, enc) {
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    const alphabetLength = this.alphabet.length;
+
+    let currentChar;
+    let keyChar;
+    let currentCharInd = 0;
+    let currentKeyInd = 0;
+
+    let keyInd = 0;
+    let newInd = 0;
+
+    let newMessage = '';
+
+    for (let i = 0; i < message.length; i++) {
+
+      currentChar = message[i];
+      currentCharInd = this.alphabet.indexOf(currentChar);
+
+      if (currentCharInd === -1) {
+        newMessage += currentChar;
+        continue;
+      }
+
+      keyChar = key[keyInd % key.length];
+      currentKeyInd = this.alphabet.indexOf(keyChar);
+
+      if (enc) {
+        newInd = (currentCharInd + currentKeyInd) % alphabetLength;
+      } else {
+        newInd = (currentCharInd - currentKeyInd + alphabetLength) % alphabetLength;
+      }
+
+      newMessage += this.alphabet[newInd];
+      keyInd++;
+    }
+
+    if (this.direct) {
+      return newMessage;
+    } else {
+      return newMessage.split('').reverse().join('');
+    }
+
   }
+
+  encrypt(message, key) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this.messageEncDec(message, key, true);
+  }
+  decrypt(message, key) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this.messageEncDec(message, key, false);
+  }
+
 }
 
 module.exports = {
